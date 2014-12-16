d025e47d7e63b91671427c165fc1a5ea7c63c7ab
NIRAJ KUMAR CHAUHAN <niraj.1991@gmail.com>
2014-12-16 23:50:03 +0530

    Dependency added

--- Customercontact/app/src/main/java/com/hewitts/com/customercontact/MainActivity.java
+++ Customercontact/app/src/main/java/com/hewitts/com/customercontact/MainActivity.java
@@ -1,17 +1,34 @@
 package com.hewitts.com.customercontact;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 
 
 public class MainActivity extends Activity {
-
+    Button addPupil,editPupil,contactPupil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addPupil = (Button) findViewById(R.id.mainmenuButton1);
+        editPupil = (Button) findViewById(R.id.mainmenubutton2);
+        contactPupil = (Button) findViewById(R.id.mainmenubutton3);
+
+
+        //add pupil click listener
+        addPupil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),PupilActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
@@ -36,4 +53,7 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
---
+++ Customercontact/app/src/main/java/com/hewitts/com/util/Pupil.java
@@ -0,0 +1,5 @@
+package com.hewitts.com.util;
+
+
+public class Pupil{
+}
